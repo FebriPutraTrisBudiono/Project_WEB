ba1e1a605fd979e89261446cca71907f5172cb54
febript <febript@gmail.com>
2020-10-15 14:04:02 +0700

    Tugas Minggu ke 4

---
+++ 15-10-2020/E41192131_Febri Putra Tris Budiono/Tugas 1 - MalasNgodding/index.php
@@ -0,0 +1,17 @@
+<?php
+//String
+/*$tes = "Saya Pergi Kerja";
+echo $tes;*/
+
+//Float
+/*$angka = 12.177;
+echo $angka;*/
+
+//Boolean
+echo true; //menghasilkan 1
+//echo false; //menghasilkan 0
+
+//array
+/*$anggota = array("Andi","Budi","Joni");
+echo $anggota[0];*/
+?>
