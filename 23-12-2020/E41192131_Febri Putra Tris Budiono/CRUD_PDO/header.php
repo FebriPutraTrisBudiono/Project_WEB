3aff4b4d7875ce9b4c44d3680f984346238ea7e6
febript <febript@gmail.com>
2020-12-29 09:42:47 +0700

    Tugas PDO

---
+++ 23-12-2020/E41192131_Febri Putra Tris Budiono/CRUD_PDO/header.php
@@ -0,0 +1,29 @@
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+  </head>
+  
+  <body class="bg-info">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="create.php">Create a person</a>
+      </li>
+      
+      
+    </ul>
+  </div>
+</nav>
